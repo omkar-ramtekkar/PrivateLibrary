b7f0e06106e6d64c4ab69b987b813dc0342022db
Omkar Ramtekkar <omkar.ramtekkar@gmail.com>
2016-11-03 09:47:15 +0530

    Initial Commit

---
+++ PrivateLibrary/PrivateLibrary.h
@@ -0,0 +1,13 @@
+//
+//  PrivateLibrary.h
+//  PrivateLibrary
+//
+//  Created by Omkar on 11/3/16.
+//  Copyright © 2016 Omkar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PrivateLibrary : NSObject
+
+@end
---
+++ PrivateLibrary/PrivateLibrary.m
@@ -0,0 +1,13 @@
+//
+//  PrivateLibrary.m
+//  PrivateLibrary
+//
+//  Created by Omkar on 11/3/16.
+//  Copyright © 2016 Omkar. All rights reserved.
+//
+
+#import "PrivateLibrary.h"
+
+@implementation PrivateLibrary
+
+@end
